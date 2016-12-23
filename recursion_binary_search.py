a18aebf21bdbd760a1f80c1400f196d5cb26c2d0
PopeOfNope <PopeOfNope@nope.com>
2016-12-22 23:30:58 -0800

    binary_search recursion

--- recursion_binary_search.py
+++ recursion_binary_search.py
@@ -1,20 +1,14 @@
-import math
-
-def binary_search(data, value, high_index=0, low_index=0):
-	""" Returns True if value is found in array. """
+def binary_search(data, value, low_index=0, high_index=0):
+	""" Returns True if value is found in array else False. """
 
 	if not data:
 		return False
 
-	midpt_index = int(math.floor((high_index + low_index) / 2))
+	if low_index > high_index:
+		return False
+
+	midpt_index = int((high_index + low_index) // 2)
 	midpt_value = data[midpt_index]
-
-	if high_index == low_index:
-		if value == midpt_value:
-			return True
-		else:
-			return False
-
 
 	if value == midpt_value:
 		return True
@@ -23,18 +17,20 @@ def binary_search(data, value, high_index=0, low_index=0):
 	elif value > midpt_value:
 		low_index = midpt_index + 1
 
-	return binary_search(data, value, high_index, low_index)
+	return binary_search(data, value, low_index, high_index)
 
 
-data = []
-print binary_search(data, 9)
+# data = []
+# print binary_search(data, 9)
+
 data= [9]
-print binary_search(data, 9, len(data)-1, 0)
+print binary_search(data, 9, 0, len(data)-1)
+
 data= [8, 8]
-print binary_search(data, 9, len(data)-1, 0)
-data = [1, 3, 3, 5, 6, 9]
-print binary_search(data, 9, len(data)-1, 0)
+print binary_search(data, 9, 0, len(data)-1)
+
+data = [1, 3, 3, 5, 6, 7]
+print binary_search(data, 9, 0, len(data)-1)
+
 data = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-print binary_search(data, 9, len(data)-1, 0)
-
-
+print binary_search(data, 9, 0,len(data)-1)
